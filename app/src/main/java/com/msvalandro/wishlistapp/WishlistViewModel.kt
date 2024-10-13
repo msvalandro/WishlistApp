8e5b748095c2fdb3c777703c00fc5bd5322455ae
Matheus Valandro <msvalandro@proton.me>
2024-10-13 20:58:47 +0100

    feat: add input fields to add/edit view

--- app/src/main/java/com/msvalandro/wishlistapp/WishlistViewModel.kt
+++ app/src/main/java/com/msvalandro/wishlistapp/WishlistViewModel.kt
@@ -1,6 +1,19 @@
 package com.msvalandro.wishlistapp
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class WishlistViewModel: ViewModel() {
+    var wishTitleState by mutableStateOf("")
+    var wishDescriptionState by mutableStateOf("")
+
+    fun onWishTitleChanged(newTitle: String) {
+        wishTitleState = newTitle
+    }
+
+    fun onWishDescriptionChanged(newDescription: String) {
+        wishDescriptionState = newDescription
+    }
 }

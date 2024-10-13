923ededf5c98fd4c6613e77122d6b1cfc4bc59fd
Matheus Valandro <msvalandro@proton.me>
2024-10-13 19:33:05 +0100

    feat: create home view and app top bar

--- app/src/main/java/com/msvalandro/wishlistapp/MainActivity.kt
+++ app/src/main/java/com/msvalandro/wishlistapp/MainActivity.kt
@@ -1,47 +1,26 @@
 package com.msvalandro.wishlistapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.msvalandro.wishlistapp.ui.theme.WishlistAppTheme
+it import com.msvalandro.wishlistapp.ui.theme.WishlistAppTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             WishlistAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    HomeView()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WishlistAppTheme {
-        Greeting("Android")
     }
 }

0b5603e2cfb44de959c2871c690be64e5d7ca3e6
Matheus Valandro <msvalandro@proton.me>
2024-10-13 19:55:44 +0100

    feat: add floating action button

--- app/src/main/java/com/msvalandro/wishlistapp/HomeView.kt
+++ app/src/main/java/com/msvalandro/wishlistapp/HomeView.kt
@@ -7,10 +7,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
@@ -20,7 +26,19 @@ fun HomeView() {
         topBar = { AppBarView(title = "Wishlist", {
             Toast.makeText(context, "Button clicked!", Toast.LENGTH_LONG).show()
         }) },
-        modifier = Modifier.fillMaxSize().windowInsetsPadding(WindowInsets.statusBars)
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                containerColor = Color.Black,
+                onClick = { /*TODO Add navigation to Add screen*/ }
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add item")
+            }
+        },
+        modifier = Modifier
+            .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.statusBars)
     ) {
         LazyColumn(modifier = Modifier
             .fillMaxSize()

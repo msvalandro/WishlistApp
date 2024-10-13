5618f572e1cb65350b59ee9be1500b0662d46a13
Matheus Valandro <msvalandro@proton.me>
2024-10-13 20:06:39 +0100

    feat: create wishlist item component

--- app/src/main/java/com/msvalandro/wishlistapp/HomeView.kt
+++ app/src/main/java/com/msvalandro/wishlistapp/HomeView.kt
@@ -1,14 +1,21 @@
 package com.msvalandro.wishlistapp
 
 import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -16,7 +23,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.msvalandro.wishlistapp.data.Wish
 
 @Composable
 fun HomeView() {
@@ -44,6 +53,23 @@ fun HomeView() {
             .fillMaxSize()
             .padding(it)) {
 
+        }
+    }
+}
+
+@Composable
+fun WithlistItem(wish: Wish, onClick: () -> Unit) {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+        .clickable {
+            onClick()
+        },
+        elevation = CardDefaults.cardElevation(10.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = wish.title, fontWeight = FontWeight.ExtraBold)
+            Text(text = wish.description)
         }
     }
 }
---
+++ app/src/main/java/com/msvalandro/wishlistapp/data/Wish.kt
@@ -0,0 +1,7 @@
+package com.msvalandro.wishlistapp.data
+
+data class Wish(
+    val id: Long = 0L,
+    val title: String = "",
+    val description: String = ""
+)

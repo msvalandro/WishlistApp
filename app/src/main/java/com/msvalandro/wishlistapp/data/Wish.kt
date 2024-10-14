625cd0f57cf571a00a19ffb87b6a432dc70b2167
Matheus Valandro <msvalandro@proton.me>
2024-10-14 18:51:46 +0100

    chore: create wishes entity

--- app/src/main/java/com/msvalandro/wishlistapp/data/Wish.kt
+++ app/src/main/java/com/msvalandro/wishlistapp/data/Wish.kt
@@ -1,8 +1,18 @@
 package com.msvalandro.wishlistapp.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "wishes")
 data class Wish(
+    @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
+
+    @ColumnInfo(name = "title")
     val title: String = "",
+
+    @ColumnInfo(name = "description")
     val description: String = ""
 )
 
